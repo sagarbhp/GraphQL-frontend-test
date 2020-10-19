113dd33c46d7f448f82aca6361f493b5e8fa991d
Sagar <sagarbhp@gmail.com>
2020-10-19 02:33:20 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  useMutation,
+  gql
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://192.168.0.185:3000/graphql",
+  cache: new InMemoryCache()
+});
+
+
+function ExchangeRates() {
+  const [updatetodo] = useMutation(gql`
+     mutation{
+      addAuthor(name:"Sagar", age:25){
+        name
+        age
+      }
+    }
+  `);
+
+
+
+  return (
+    <button onClick={()=>{updatetodo()}}>click me</button>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <ExchangeRates />
+    </ApolloProvider>
+ 
   );
 }
 
